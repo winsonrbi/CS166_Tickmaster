96167e6eabc83ef4902f24186055df43444409cb
K Sand <35273571+ksand012@users.noreply.github.com>
2020-06-13 03:00:00 -0700

    Fix RemovePayment to make reserved seat bid NULL

--- java/src/Ticketmaster.java
+++ java/src/Ticketmaster.java
@@ -714,8 +714,10 @@ public class Ticketmaster{
 				return;
 			}
 
-			query = "UPDATE bookings SET status = 'Canceled' WHERE bid = '" + bid + "'";
-			esql.executeUpdate(query);	
+			query = "UPDATE bookings SET status = 'Cancelled' WHERE bid = '" + bid + "'";
+			esql.executeUpdate(query);
+			query = "UPDATE showseats SET bid = NULL WHERE bid = '" + bid + "'";
+			esql.executeUpdate(query);
 			System.out.println("Bookings ID " + bid + " has been successfully cancelled."); 	
 		}
 		catch(Exception e){
